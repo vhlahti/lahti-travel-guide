0941b57d05820e0f5f1aa7f1f96d668e912d1e5b
vhlahti <henri.veholahti@gmail.com>
2025-10-20 16:40:36 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lahti-travel-guide',
+  webDir: 'www'
+};
+
+export default config;
