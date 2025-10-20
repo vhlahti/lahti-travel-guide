58c8010b7b5b0b32de6b51b0e3cbe76cb3236bbe
vhlahti <henri.veholahti@gmail.com>
2025-10-20 20:54:48 +0300

    Add tabs interface

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'tabs',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'tabs',
+    loadComponent: () => import('./pages/tabs/tabs.page').then( m => m.TabsPage)
   },
 ];
