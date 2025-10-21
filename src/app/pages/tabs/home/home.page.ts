7d8b28d3821321392157eab9f00e6cd9d7310504
vhlahti <henri.veholahti@gmail.com>
2025-10-21 16:54:20 +0300

    Add home page

---
+++ src/app/pages/tabs/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonLabel, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonLabel, IonToolbar, CommonModule, FormsModule]
+})
+export class HomePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
