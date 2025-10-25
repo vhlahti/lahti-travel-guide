74f60b2b4dcb654a187489413fd6832123da71dc
vhlahti <henri.veholahti@gmail.com>
2025-10-25 19:39:06 +0300

    Add item-detail logic

--- src/app/pages/tabs/item-detail/item-detail.page.ts
+++ src/app/pages/tabs/item-detail/item-detail.page.ts
@@ -1,20 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonToolbar, IonButtons, IonBackButton, IonSpinner } from '@ionic/angular/standalone';
+import { Observable, map } from 'rxjs';
+import { Product } from 'src/app/interfaces/product.interface';
+import { Media } from 'src/app/services/media';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.page.html',
   styleUrls: ['./item-detail.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButtons,
+    IonBackButton,
+    IonSpinner
+  ]
 })
 export class ItemDetailPage implements OnInit {
+  product$!: Observable<Product | undefined>;
 
-  constructor() { }
+  constructor(private media: Media, private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    // Resolve blocked aria-hidden error
+    const activeElement = document.activeElement as HTMLElement;
+    activeElement?.blur();
+    
+    // Compare id
+    const id = this.route.snapshot.paramMap.get('id');
+    this.product$ = this.media.getProductsInLahtiList().pipe(
+      map(products => products.find(p => p.id === id))
+    );
+
   }
 
 }
