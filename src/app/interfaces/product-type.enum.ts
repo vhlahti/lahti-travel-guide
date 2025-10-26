496f7b4dd0a5f4e2b693dbad1373e5d43409ab6b
vhlahti <henri.veholahti@gmail.com>
2025-10-26 20:23:44 +0200

    Add media card filtering logic

---
+++ src/app/interfaces/product-type.enum.ts
@@ -0,0 +1,11 @@
+export enum ProductType {
+  Accommodation = 'accommodation',
+  Attraction = 'attraction',
+  Experience = 'experience',
+  Event = 'event',
+  RentalService = 'rental_service',
+  Restaurant = 'restaurant',
+  Shop = 'shop',
+  Transportation = 'transportation',
+  Venue = 'venue'
+}
