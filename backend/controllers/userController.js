042eecfe4c8f07459910b4060e2b8a63ab18d2eb
vhlahti <henri.veholahti@gmail.com>
2025-11-03 22:06:35 +0200

    Add user password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require("bcryptjs");
 
 const registerUser = async (req, res) => {
     const { username, password, confirmPassword } = req.body;
@@ -15,9 +16,12 @@ const registerUser = async (req, res) => {
             return res.status(400).json({ message: 'Passwords do not match' });
         }
 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
         const user = await User.create({
         username,
-        password: password,
+        password: hashedPassword,
         });
 
         res.status(201).json({
