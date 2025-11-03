41a8797fd88ddd49a52045542ac5b4f765c7a664
vhlahti <henri.veholahti@gmail.com>
2025-11-03 17:33:32 +0200

    Connect register user logic to MongoDB database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,33 @@
 const User = require('../models/User');
 
 const registerUser = async (req, res) => {
-res.status(201).json({message:'registerUser'});
+    const { username, password, confirmPassword } = req.body;
+    try {
+        if (!username || !password || !confirmPassword) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+
+        const userExists = await User.findOne({ username });
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        if (password !== confirmPassword) {
+            return res.status(400).json({ message: 'Passwords do not match' });
+        }
+
+        const user = await User.create({
+        username,
+        password: password,
+        });
+
+        res.status(201).json({
+           id: user._id,
+           username: user.username,
+        });
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 module.exports = {
