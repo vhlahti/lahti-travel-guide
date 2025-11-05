24e098eefb783f2aabd5fe7bf45dc1bf2a0a8110
vhlahti <henri.veholahti@gmail.com>
2025-11-05 18:01:53 +0200

    Create profile, login and register reusable components

--- src/app/pages/tabs/account/account.page.ts
+++ src/app/pages/tabs/account/account.page.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ProfileComponent } from 'src/app/components/profile/profile.component';
+import { LoginComponent } from 'src/app/components/login/login.component';
+import { RegisterComponent } from 'src/app/components/register/register.component';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
   styleUrls: ['./account.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, ProfileComponent, LoginComponent, RegisterComponent]
 })
 export class AccountPage implements OnInit {
 
