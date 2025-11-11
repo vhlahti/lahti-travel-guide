5c3b8240786faffc4fd4825584e6e5dd4579e9eb
vhlahti <henri.veholahti@gmail.com>
2025-11-11 19:00:03 +0200

    Rebuild before main merge

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
 import { MediaCardComponent } from 'src/app/components/media-card/media-card.component'
-
 
 @Component({
   selector: 'app-home',
@@ -11,7 +10,6 @@ import { MediaCardComponent } from 'src/app/components/media-card/media-card.com
   styleUrls: ['./home.page.scss'],
   standalone: true,
   imports: [
-    IonContent,
     IonHeader,
     IonToolbar,
     CommonModule,
